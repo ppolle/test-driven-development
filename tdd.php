871e896d83de40ec15d136aae3adc4ce03c27075
peter polle <peter.m.polle@gmail.com>
2015-09-16 19:58:29 +0300

    new commit

---
+++ tdd.php
@@ -0,0 +1,30 @@
+<?php
+
+$tester = new testarea();
+echo $tester->getarea();
+
+class area{
+
+	function area(){}
+	function getarea($length,$width){
+		return $length*$width;
+	}
+}
+
+
+
+class testarea{
+	var $areaclass;
+	function testarea(){
+	$this->areaclass=new area();
+	}
+	function testgetarea(){
+	$result = $this->areaclass->getarea(10,12);
+  
+  if ($result==120){
+	  
+  	echo "your test has worked";
+        }else{
+       echo "your test has not worked";
+}
+}
